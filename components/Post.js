a352d00045a5c5f6dfb51cbb7eb2de9d35e758d8
PaulinaRog <rog.paulina@gmail.com>
2023-06-09 16:07:48 +0200

    alt added

--- components/Post.js
+++ components/Post.js
@@ -11,6 +11,7 @@ export default function Post({ post }) {
           width={300}
           height={150}
           className="mb-8 object-cover w-full h-full opacity-80"
+          alt={`${post.title} ${post.title_second_line}`}
         />
       </div>
       <Link href={`blog/${post.slug}`} className="text-[18px] font-bold mb-3 mt-5 pl-5 pr-5 block">
